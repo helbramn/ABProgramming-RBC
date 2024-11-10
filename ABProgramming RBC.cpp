92734a1b422bbf6d83f191cbf61fb3684c6f8dd7
helbramn <barabasrori@gmail.com>
2024-11-10 21:14:51 +0100

    Agregar archivos de proyecto.

---
+++ ABProgramming RBC.cpp
@@ -0,0 +1,12 @@
+﻿// ABProgramming RBC.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ABProgramming RBC.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
